a69a39dc9a7a3048adf61fcc6c413cf613238b7b
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-11-13 00:16:13 +0300

    Updates

---
+++ precache-manifest.58a539447c711e43903f4c81cc4bdacb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7451439fe0408e8e2e3e467357706726",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7c43d0ce29b523dbe486",
+    "url": "/weather-app/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "16862f38a2d0f7708c5b",
+    "url": "/weather-app/static/css/main.288d2eca.chunk.css"
+  },
+  {
+    "revision": "7c43d0ce29b523dbe486",
+    "url": "/weather-app/static/js/2.2e1d5ef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e1d5ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16862f38a2d0f7708c5b",
+    "url": "/weather-app/static/js/main.2e66c706.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
